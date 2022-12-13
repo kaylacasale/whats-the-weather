517f3b31e56933b80bcc50b4b32ac846b744256f
Kayla Casale <kayla.casale@gmail.com>
2022-12-13 03:44:39 -0800

    added styling like box shadows

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,6 @@ var timePEl = document.querySelector(".time");
 
 var time = dayjs().format('h:mm A');
 timePEl.textContent = time;
+
+var navEl = document.querySelector("nav");
+//navEl.setAttribute('class', 'shadow-lg row')
