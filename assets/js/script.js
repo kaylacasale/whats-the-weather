aa46da7077b60547d0d5dfc015995615e6b83bd3
Kayla Casale <kayla.casale@gmail.com>
2022-12-13 03:34:23 -0800

    added current date and time

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var datePEl = document.querySelector(".date");
+var today = dayjs().format('MMM D, YYYY');
+datePEl.textContent = today;
+
+var timePEl = document.querySelector(".time");
+
+var time = dayjs().format('h:mm A');
+timePEl.textContent = time;
