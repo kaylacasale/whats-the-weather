6050df7eb5852afc4e4463d2dcf7e349a28e1328
Kayla Casale <kayla.casale@gmail.com>
2022-12-13 22:41:30 -0800

    added description and temperature data with styling enhancements to text

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,9 @@ var searchBarEl = document.getElementById("search-bar");
 //* grab search-bar-btn by id #search-bar-btn
 var searchBtnEl = document.getElementById("search-bar-btn");
 
+
+
+
 var apiKey = "981e313affd3213b334e9460a4970735";
 var cityName;
 
@@ -38,14 +41,14 @@ searchBtnEl.addEventListener('click', function () {
     console.log(searchBarEl.value);
 
     city = searchBarEl.value;
-    state = searchBarEl.value;
+    //state = searchBarEl.value;
     latlon();
 })
 
 //* function to find 'lat' and 'lon' (global) and store in var 'coordinates' (local)
 //* pass coordinates from geo identifier api into next promise to get 'lat' and 'lon' from searchbar input
 var latlon = function () {
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=1&appid=981e313affd3213b334e9460a4970735`)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},US&limit=1&appid=981e313affd3213b334e9460a4970735`)
 
 
         .then(response => response.json())
@@ -57,39 +60,142 @@ var latlon = function () {
             console.log(data[0].country)
             console.log(data[0].name)
             console.log(data[0].state)
+            state = data[0].state;
+            country = data[0].country;
+            console.log(state)
 
             var coordinates = [lat, lon]
             console.log(coordinates)
             //*coordinate[i]
 
 
-            infolatlon(coordinates);
-            return coordinates
+            //infolatlonFuture(coordinates, state);
+            infolatlonNow(coordinates, state, country);
+            return coordinates, state, country
         })
 
 
 }
 
-var location = document.getElementById("main-location");
-var temperature = document.getElementById("main-wind");
-var humidity = document.getElementById("main-humidity");
-
-var infolatlon = function (coordinates) {
+var infolatlonNow = function (coordinates, state) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=981e313affd3213b334e9460a4970735`)
-
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            console.log(coordinates)
+            console.log(data);
             console.log(data.main)
-            console.log(data.main.feels_like)
-            //* degrees in Kelvin
-            var feelsLikeKelvin = data.main.feels_like;
-            //* degrees in Farenheit
-            var feelsLikeFarenheit = (feelsLikeKelvin - 273.15) * (9 / 5) + 32;
-            console.log(feelsLikeFarenheit);
+            //console.log(data.city.country)
+            console.log(state)
+
+
+            //country = data.city.country;
+
+
+            console.log(data.name)
+            cityName = data.name;
+            var cityStateCountry = document.querySelector('#location')
+            cityStateCountry.textContent = cityName + ', ' + state + ', ' + country;
+
+            console.log(data.main.temp)
+            //* temp now in Kelvin
+            var tempKelvin = data.main.temp;
+            //* temp now in Farenheit
+            var tempFarenheit = Math.floor((tempKelvin - 273.15) * (9 / 5) + 32);
+            var temperature = document.querySelector("#main-temperature");
+            temperature.textContent = 'Temperature: ' + tempFarenheit + '℉';
+
+
+            //* icon
+            // console.log(data.weather[0].icon)
+            // icon = data.weather[0].icon.png
+            // //var iconSpan = document.querySelector('#icon');
+            // var iconNew = document.createElement("img");
+            // iconNew.setAttribute("src", "icon")
+            // cityStateCountry.append(iconNew)
+
+
+            //* data.weather[0]; start using El in var name when grabbing elements
+            var descriptionLi = document.querySelector("#main-description");
+            console.log(data.weather[0].description);
+            // descriptionNow = data.weather[0].description.value;
+            descriptionLi.textContent = 'Description: ' + data.weather[0].description;
+
+
+
+
+            // console.log(data.list)
+            // console.log(data.list[0].dt_txt)
+            // var datetime = data.list[0].dt_txt;
+            // console.log(dayjs(datetime).format('MMM D, YYYY h:mm A'));
+            // //* date and time weather forecasted for
+            // futureDayTime = dayjs(datetime).format('MMM D, YYYY h:mm A');
+            // console.log(futureDayTime)
         })
 
+
 }
+
+var infolatlonFuture = function (coordinates, state) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=981e313affd3213b334e9460a4970735`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(coordinates)
+            console.log(data);
+            console.log(data.city)
+            console.log(data.city.country)
+            console.log(state)
+            country = data.city.country;
+
+            console.log(data.city.name)
+            cityName = data.city.name;
+            //var cityStateCountry = document.querySelector('#location')
+            cityStateCountry.textContent = cityName + country;
+
+            console.log(data.list)
+            console.log(data.list[0].dt_txt)
+            var datetime = data.list[0].dt_txt;
+            console.log(dayjs(datetime).format('MMM D, YYYY h:mm A'));
+            //* date and time weather forecasted for
+            futureDayTime = dayjs(datetime).format('MMM D, YYYY h:mm A');
+            console.log(futureDayTime)
+
+
+            console.log(data.list[0].main.temp)
+            //* temp in Kelvin
+            var tempKevlin = data.list[0].main.temp;
+            //* temp in Farenheit
+            var tempFarenheit = (tempKevlin - 273.15) * (9 / 5) + 32;
+            console.log(tempFarenheit);
+
+
+
+        })
+}
+
+
+
+
+
+// var location = document.getElementById("main-location");
+// var temperature = document.getElementById("main-wind");
+// var humidity = document.getElementById("main-humidity");
+
+// var infolatlon = function (coordinates) {
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e7a88aed42c57808fa6dc34c1ede065e`)
+
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data)
+//             console.log(data.main)
+//             console.log(data.main.feels_like)
+//             //* degrees in Kelvin
+//             var feelsLikeKelvin = data.main.feels_like;
+//             //* degrees in Farenheit
+//             var feelsLikeFarenheit = (feelsLikeKelvin - 273.15) * (9 / 5) + 32;
+//             console.log(feelsLikeFarenheit);
+//         })
+
+// }
 
 
 
